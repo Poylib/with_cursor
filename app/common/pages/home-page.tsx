d4bd7d50c4b086f3cabe2c4854970f9913eb82c0
poylib <poy_lib@naver.com>
2025-02-07 10:53:24 +0900

    Add: setting Shadcn

---
+++ app/common/pages/home-page.tsx
@@ -0,0 +1,12 @@
+import { type FC } from "react";
+
+const HomePage: FC = () => {
+    return (
+        <div className="p-4">
+            <h1 className="text-2xl font-bold">Welcome to the Home Page</h1>
+            <p>This is the home page of our application.</p>
+        </div>
+    );
+};
+
+export default HomePage; 
